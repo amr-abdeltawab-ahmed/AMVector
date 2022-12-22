41216550e479c457d0031de5c3abd50246358b4e
amr-abdeltawab-ahmed <101482153+amr-abdeltawab-ahmed@users.noreply.github.com>
2022-12-22 15:51:47 +0200

    copy, move and some functions

--- AMVector.cpp
+++ AMVector.cpp
@@ -55,7 +55,89 @@ public:
     ~AMVector() {
         delete[] data;
     }
-    
+
+    // Access operations
+    T& operator[](int index){
+        if(index > size-1){
+            cout << "An error has occured, index out of the range." << endl;
+            exit(-1);
+        }
+        return data[index];
+    }
+
+    // Copy assignment
+    AMVector &operator=(const AMVector& other){
+        size = other.getSize();
+        capacity = other.get_capacity();
+        data = new T[capacity];
+        for(int i = 0; i < other.getSize(); i++){
+            data[i] = other.data[i];
+        }
+    }
+
+    // Move assignment
+    AMVector &operator=(const AMVector&& other){
+        if (this != &other){
+            size = other.getSize();
+            capacity = other.get_capacity();
+            data = new T[capacity];
+            for (int i = 0; i < other.getSize(); i++){
+                data[i] = other.data[i];
+            }
+            //do clear for other
+            other.size = 0;
+            other.capacity = 0;
+            other.data = nullptr;
+        }
+        return *this;
+    }
+
+
+    T& operator=(const T& other){
+        if(this != &other){
+            delete[] data;
+            size = other.size;
+            capacity = other.capacity;
+            data = new T[capacity];
+            for(int i = 0; i < other.size; i++){
+                data[i] = other.data[i];
+            }
+        }
+        else{
+            cout << "Cannot delete self" << endl;
+        }
+    }
+
+
+    //push_back and pop_back
+    void push_back(T item){
+        if(size < capacity){
+            data[size++] = item;
+        }
+        else{
+            T* newdata = new T[capacity*2];
+            capacity *= 2;
+            for(int i = 0; i < size; i++){
+                newdata[i] = data[i];
+            }
+            delete[] data;
+            data = newdata;
+            newdata = nullptr;
+            data[size++] = item;
+        }
+    }
+    T pop_back(){
+        if (size > 0)  {
+            return data[size-- - 1];
+        }
+        else {
+            cout << "nothing to pop";
+        }
+    }
+
+
+
+
 };
 
 
